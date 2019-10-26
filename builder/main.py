c8dc706448e149859e6c87020bd062240cb78f3d
Miodrag Milanovic <mmicko@gmail.com>
2019-10-26 16:50:19 +0200

    Added copyright message

--- builder/main.py
+++ builder/main.py
@@ -2,6 +2,7 @@
 # Copyright 2014-present PlatformIO <contact@platformio.org>
 # Copyright 2016-present Juan González <juan@iearobotics.com>
 #                        Jesús Arroyo Torrens <jesus.jkhlg@gmail.com>
+# Copyright 2019-present Miodrag Milanovic <mmicko@gmail.com>
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
